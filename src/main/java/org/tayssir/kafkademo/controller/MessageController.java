7da3b7ab84f3edb1d71a24b9cc3ebe9493922bf3
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-06-10 14:33:51 +0100

    [ADDED] producer and controller

---
+++ src/main/java/org/tayssir/kafkademo/controller/MessageController.java
@@ -0,0 +1,21 @@
+package org.tayssir.kafkademo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tayssir.kafkademo.producer.KafkaProducer;
+
+@RestController
+@RequestMapping("/api/tsts/messages")
+@RequiredArgsConstructor
+public class MessageController {
+    private final KafkaProducer producer;
+    @PostMapping
+    public ResponseEntity<String> sendMessage(@RequestBody String message){
+        producer.publishMessage(message);
+        return ResponseEntity.ok("Message queued successfully");
+    }
+}
---
+++ src/main/java/org/tayssir/kafkademo/producer/KafkaProducer.java
@@ -0,0 +1,18 @@
+package org.tayssir.kafkademo.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+    private final KafkaTemplate<String,String> kafkaTemplate;
+    public void publishMessage(String msg){
+        log.info(String.format("sending message to testes:: %s", msg));
+        kafkaTemplate.send("testes", msg);
+
+    }
+}
